e22c1ad9d5492ee75d99cc2982c07e0d8a16594d
vignesh06 <vigneshsn@panainfotech.com>
2019-07-31 22:22:46 +0530

    Captcha Recognition

---
+++ README.md
@@ -0,0 +1,21 @@
+# captcha-resolver-python
+This project hosts the python code which is trains the captcha resolver and provides the output .dat and .hdf5 files
+
+Steps to train the captcha model
+
+1. Clone the project to your local folder.
+
+2. Unzip the generated_captcha_images.zip in the same folder.
+
+3. Create a new folder by name extracted_letter_images in the folder where train_model.py is present.
+
+4. Before running the requirements.txt have pyhton 3 installed in your system.
+
+5. From the command prompt navigate to folder where extract_single_letters_from_captchas.py is present and then run "python extract_single_letters_from_captchas.py".
+
+6. Once the execution of the above command is done then run "python train_model.py" .
+
+7. After the execution of  "python train_model.py" command this will create "captcha_model.hdf5" and "model_labels.dat" files in the same folder and 
+later this file will be used for predicting the letters in the ecourt captcha image(python train_model.py execution will take 5 to 10 minutes for completion) .
+
+8. To test model run "python solve_captchas_with_model.py".
---
+++ extract_single_letters_from_captchas.py
@@ -0,0 +1,94 @@
+import os
+import os.path
+import cv2
+import glob
+import imutils
+import numpy as np
+
+
+CAPTCHA_IMAGE_FOLDER = "generated_captcha_imagess"
+OUTPUT_FOLDER = "extracted_letter_images"
+
+
+# Get a list of all the captcha images we need to process
+captcha_image_files = glob.glob(os.path.join(CAPTCHA_IMAGE_FOLDER, "*"))
+counts = {}
+
+# loop over the image paths
+for (i, captcha_image_file) in enumerate(captcha_image_files):
+    print("[INFO] processing image {}/{}".format(i + 1, len(captcha_image_files)))
+
+    # Since the filename contains the captcha text (i.e. "2A2X.png" has the text "2A2X"),
+    # grab the base filename as the text
+    filename = os.path.basename(captcha_image_file)
+    captcha_correct_text = os.path.splitext(filename)[0]
+
+    # Load the image and convert it to grayscale
+    img = cv2.imread(captcha_image_file)
+    
+    lower = np.array([0,0,0])  #-- Lower range --
+    upper = np.array([50,50,50])  #-- Upper range --
+    mask = cv2.inRange(img, lower, upper)
+    res = cv2.bitwise_and(img, img, mask= mask)  #-- Contains pixels having the gray color--
+    img = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 3))
+    dilation_1 = cv2.dilate(img,kernel,iterations = 2)
+    erosion_1 = cv2.erode(dilation_1,kernel,iterations =2)
+    gray = cv2.copyMakeBorder(erosion_1, 8, 8, 8, 8, cv2.BORDER_REPLICATE)
+
+    # threshold the image (convert it to pure black and white)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    gray = cv2.copyMakeBorder(thresh.copy(), 8, 8, 8, 8, cv2.BORDER_REPLICATE)
+
+    # threshold the image (convert it to pure black and white)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    
+     #find the contours (continuous blobs of pixels) the image
+    contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Hack for compatibility with different OpenCV versions
+    contours = contours[0] if imutils.is_cv2() else contours[1]
+
+    letter_image_regions = []
+
+    # Now we can loop through each of the four contours and extract the letter
+    # inside of each one
+    for contour in contours:
+        # Get the rectangle that contains the contour
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        letter_image_regions.append((x, y, w, h))
+
+    # If we found more or less than 4 letters in the captcha, our letter extraction
+    # didn't work correcly. Skip the image instead of saving bad training data!
+    if len(letter_image_regions) != 5:
+        continue
+
+    # Sort the detected letter images based on the x coordinate to make sure
+    # we are processing them from left-to-right so we match the right image
+    # with the right letter
+    letter_image_regions = sorted(letter_image_regions, key=lambda x: x[0])
+    print(letter_image_regions)
+
+    # Save out each letter as a single image
+    for letter_bounding_box, letter_text in zip(letter_image_regions, captcha_correct_text):
+        # Grab the coordinates of the letter in the image
+        x, y, w, h = letter_bounding_box
+
+        # Extract the letter from the original image with a 2-pixel margin around the edge
+        letter_image = gray[y - 2:y + h + 2, x - 2:x + w + 2]
+
+        # Get the folder to save the image in
+        save_path = os.path.join(OUTPUT_FOLDER, letter_text)
+
+        # if the output directory does not exist, create it
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        # write the letter image to a file
+        count = counts.get(letter_text, 1)
+        p = os.path.join(save_path, "{}.png".format(str(count).zfill(6)))
+        cv2.imwrite(p, letter_image)
+
+        # increment the count for the current key
+        counts[letter_text] = count + 1
